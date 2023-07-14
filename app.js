2497ac718a8f8d6ffb54d5344d4f4e4dd2860839
lauqui08 <quismorio27@gmail.com>
2023-07-15 00:03:09 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {join} = require('node:path');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+app.use(express.static(join(__dirname,'node_modules','bootstrap','dist')));
+app.use(express.static(join(__dirname,'node_modules','bootstrap-icons','font')));
+app.use(express.static('./public'));
+
+
+//template engine
+app.set('view engine','ejs');
+app.use(expressLayouts);
+app.set('layout','./pages/index');
+
+
+app.get('', (req,res) => {
+    res.render('home');
+});
+
+
+app.listen(8080);
