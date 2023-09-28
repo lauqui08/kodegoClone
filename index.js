9cf9621151cccb3bd1854f8711cdf6f04a479e06
Laurence Quismorio <quismorio27@gmail.com>
2023-09-28 11:21:44 +0800

    vercel

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const {join} = require('node:path');
-const expressLayouts = require('express-ejs-layouts');
+const express = require("express");
+const { join } = require("node:path");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
-app.use(express.static(join(__dirname,'node_modules','bootstrap','dist')));
-app.use(express.static(join(__dirname,'node_modules','bootstrap-icons','font')));
-app.use(express.static('./public'));
-
+app.use(express.static(join(__dirname, "node_modules", "bootstrap", "dist")));
+app.use(
+  express.static(join(__dirname, "node_modules", "bootstrap-icons", "font"))
+);
+app.use(express.static("./public"));
 
 //template engine
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set('layout','./pages/index');
+app.set("layout", "./pages/index");
 
-
-app.get('', (req,res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
-
 
 app.listen(8080);
